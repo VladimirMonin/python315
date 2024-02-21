86c0898723db960d42fbd67a6b7c1993875547c8
v <v@mail.ru>
2024-02-21 19:38:49 +0600

    lesson_20: docstring - разрешаем конфликт слияния

--- PYTHON/lesson_20.py
+++ PYTHON/lesson_20.py
@@ -3,14 +3,24 @@ Lesson 20
 21.02.2024
 Git и GitHub
 
+Разрешаем конфликт слияния
 """
 
 
 def say_hello(name: str) -> None:
+    """
+    Приветствие пользователя по имени
+    :param name: имя пользователя
+    :return: строка приветствия
+    """
     print(f"Hello, {name}!")
 
 
 def get_name() -> str:
+    """
+    Получение имени пользователя
+    :return: имя пользователя
+    """
     name = input("Enter your name: ")
     return name
 
