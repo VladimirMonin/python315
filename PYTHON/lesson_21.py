d5b2f36ee1975cc49ddd2902e08c78239c1e7c1f
v <v@mail.ru>
2024-02-26 18:40:36 +0600

    lesson_21: Пробуем изменить аттрибуты класса

--- PYTHON/lesson_21.py
+++ PYTHON/lesson_21.py
@@ -9,6 +9,7 @@
 # Синтаксис - class <Имя класса>:
 class LadaCar:
     price = 100500
+    color = 'black'
 
 
 # Нейминг классов - UpperCamelCase - каждое слово с большой буквы
@@ -24,3 +25,12 @@ lada3 = LadaCar()
 
 # Доступ к атрибутам класса
 print(lada.price)
+
+# Изменение атрибутов класса
+lada.color = 'red'
+lada.price = 1
+print(lada.color)
+print(lada.price)
+
+print(lada2.color)
+print(lada2.price)
