00636dc0c1924c1c3c8c509697a51cdb90dfaf6f
v <v@mail.ru>
2024-02-28 19:31:21 +0600

    lesson_22: __len__

--- PYTHON/lesson_22.py
+++ PYTHON/lesson_22.py
@@ -15,107 +15,29 @@ Lesson 22
 """
 
 
-# Напишем класс Person, который будет содержать информацию о человеке
+# __len__ - метод для определения длины объекта
 
-class Person:
-    def __init__(self, name: str, age, address: str):
-        self.name = name
-        self.age = age
-        self.address = address
+# класс Труба. Макрка, материал, диаметр, длина
 
-    def __str__(self) -> str:
-        """
-        Метод для представления объекта в виде строки
-        Используется для вывода информации о объекте через print()
-        :return:
-        """
-        return f"Информация о человеке: {self.name}, {self.age}, {self.address}"
+class Pipe:
+    # Допустимые марки в атрибуте класса
+    def __init__(self, mark: str, material: str, diameter: int, length: int):
+        self.mark = mark
+        self.material = material
+        self.diameter = diameter
+        self.length = length
 
-    def __repr__(self) -> str:
-        """
-        Метод для представления объекта в виде строки
-        Используется для возможности воссоздать объект через eval()
-        Вторично: для вывода информации о объекте через print()
-        :return:
-        """
-        return f"{self.__class__.__name__}('{self.name}', '{self.age}', '{self.address}')"
-
-
-# Создадим экземпляр класса Person
-person = Person('Николай', 25, 'USA')
-person2 = Person("Абрахам", 35, "Канада")
-
-persons = [person, person2]
-
-print(person)
-print(person2)
-
-[print(person) for person in persons]
-
-print(dir(person))
-
-# Получим значение __repr__ для person
-repr_str = repr(person)
-print(repr_str)
-# Воссоздадим объект person из строки
-# Person('Николай', 25, 'USA')
-# Person(Николай, 25, USA)
-
-new_person = eval(repr_str)
-print(new_person)
-# Практика
-"""
-Касса самообслуживания
-1. Опишите класс Product, который будет содержать информацию о товаре:
-- инициализатор
-- Название
-- Цена
-__str__
-
-Опишите цикл ввода товаров в кассу, который будет принимать название и цену товара.
-На каждой итерации создавайте экземпляр продукта и складывайте его в список продуктов.
-
-2. Если пользователь вводит ничего, цикл останавливается и выводит список продуктов.
-3. Если пользователь вводит более 4 продуктов - raise ValueError - Можно не более 4 продуктов
-"""
-
-
-class Product:
-    """
-    Класс для создания товаров
-    """
-    def __init__(self, name: str, price: float):
-        """
-        Инициализатор класса
-        :param name: Название товара
-        :param price: Цена товара
-        """
-        self.name = name
-        self.price = price
+    def __len__(self) -> int:
+        return self.length
 
     def __str__(self) -> str:
-        return f"Товар: {self.name}, Цена: {self.price}"
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}('{self.name}', '{self.price}')"
+        return f'Труба {self.mark} из {self.material} диаметром {self.diameter} мм длиной {self.length} м'
 
 
-products = []
-PRODUCT_LIMIT = 4
+pipe = Pipe('Труба', 'Сталь', 100, 500)
+pipe2 = Pipe('Труба', 'Сталь', 100, 1000)
 
-while True:
-    if len(products) > PRODUCT_LIMIT:  # Если пользователь ввел более 4 продуктов
-        raise ValueError(f"Можно не более {PRODUCT_LIMIT} продуктов")
+print(len(pipe))  # 500
+print(len(pipe2))  # 1000
 
-    name = input("Введите название товара: ")  # Вводим название товара
-
-    if not name:  # Если пользователь ввел пустую строку
-        break
-
-    price = float(input("Введите цену товара: "))  # Вводим цену товара
-
-    product = Product(name, price)  # Создаем экземпляр класса Product
-    products.append(product)
-
-print(products) # Это будет принт repr
-[print(product) for product in products]  # Это будет принт str
+print(len(pipe) > len(pipe2))  # False
