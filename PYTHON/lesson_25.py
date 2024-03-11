f79ce619175d1722eef9095555ff4055ab6009b4
v <v@mail.ru>
2024-03-11 17:13:43 +0500

    lesson_25: __eq__

--- PYTHON/lesson_25.py
+++ PYTHON/lesson_25.py
@@ -25,6 +25,7 @@ class Kettlebell:
     """
     Гиря.
     """
+
     def __init__(self, weight, length=40, width=10):
         self.weight = weight
         self.length = length
@@ -36,13 +37,24 @@ class Kettlebell:
     def __bool__(self) -> bool:
         return self.weight > 0
 
-    # def __len__(self) -> int:
-    #     return self.length
+    def __len__(self) -> int:
+        return self.weight
+
+    def __eq__(self, other: 'Kettlebell') -> bool:
+        # Проверка на принадлежность к классу
+        if not isinstance(other, Kettlebell):
+            raise ValueError('Неверный тип')
+        return self.weight == other.weight and self.length == other.length and self.width == other.width
 
 
-ket1 = Kettlebell(16)
+ket1 = Kettlebell(16, 45, 11)
 print(ket1)
 if ket1:
     print('Гиря настоящая')
 
 print(len(ket1))
+
+ket2 = Kettlebell(16)
+ket3 = Kettlebell(24)
+
+print(ket1 == ket2) # TODO - Как сравниваются объекты автоматически?
