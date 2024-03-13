b3d286900c157dc9967670bfbbc6deb0469124c8
v <v@mail.ru>
2024-03-13 18:32:26 +0500

    lesson_26: класс BankAccount и пример математики BankAccount

--- PYTHON/Lesson_26.py
+++ PYTHON/Lesson_26.py
@@ -19,20 +19,28 @@ __imul__ - умножение
 __itruediv__ - деление
 
 """
+from typing import Union
 
 
 # Класс банковский счет с методом __add__ и __iadd__
 class BankAccount:
-    def __init__(self, balance: float):
+    def __init__(self, balance: float, owner: str):
         self.balance = balance
+        self.owner = owner
 
     def __str__(self) -> str:
-        return f'Банковский счет: {self.balance}'
+        return f'Банковский счет: {self.balance}, принадлежит: {self.owner}'
 
-    def __add__(self, other: float | int) -> "BankAccount":
+    def __add__(self, other: Union[float, int, "BankAccount"]) -> "BankAccount":
         if isinstance(other, (int, float)):
             self.balance += other
             return self
+        elif isinstance(other, BankAccount):
+            new_balance = self.balance + other.balance
+            new_owners = self.owner + " и " + other.owner
+            self.balance = 0
+            other.balance = 0
+            return BankAccount(new_balance, new_owners)
 
     def __sub__(self, other: float | int) -> "BankAccount":
         if isinstance(other, (int, float)):
@@ -51,7 +59,7 @@ class BankAccount:
 
 
 # Создаем объекты
-my_account = BankAccount(1000)
+my_account = BankAccount(1000, 'Петров')
 print(my_account)
 
 # Сложение
@@ -64,5 +72,20 @@ print(type(my_account))
 
 a = 400
 a += 500
-a + 1000
+
 print(a)
+
+# Сложение аккаунтов
+b1 = BankAccount(1000, 'Иванов')
+b2 = BankAccount(1000, 'Смирнова')
+
+b4 = BankAccount(1000, 'Петров')
+
+b3 = b2 + b1 + b4
+
+"""
+Как происходит сложение двух, и более чисел?
+Создаются ли новые экземпляры класса Int?
+Как происходит сложение двух, и более экземпляров класса BankAccount?
+"""
+print(b3)
