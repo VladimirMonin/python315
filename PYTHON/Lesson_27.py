bbb800c006da2d28458b57a7a67fa801a56e078e
v <v@mail.ru>
2024-03-18 18:56:21 +0500

    lesson_27: dataclasses default_factory list

--- PYTHON/Lesson_27.py
+++ PYTHON/Lesson_27.py
@@ -6,6 +6,7 @@ Dataclasses
 - как определяется repr
 - как определяется eq
 - сериализация и десериализация
+- field(default_factory=list) - для изменяемых типов
 """
 from dataclasses import dataclass, field
 
@@ -18,14 +19,13 @@ class Person:
     name: str
     age: int = field(compare=False)
     city: str = field(compare=False)
+    hobbies: list = field(default_factory=list)
 
 
-p1 = Person("Вася", 20, "Архангельск")
-p2 = Person("Маша", 18, "Санкт-Петербург")
-p3 = Person("Вася", 20, "Москва")
-p4 = Person("Вася", 21, "Москва")
+p1 = Person("Иван", 35, "Москва", ["плавание", "путешествия"])
+p2 = Person("Анна", 28, "Санкт-Петербург", ["рафтинг", "манга"])
+p1.hobbies.append("программирование")
+p2.hobbies.append("JS")
 
-p1_str = str(p1)
-p1_new = eval(p1_str)
-
-print(p1 == p4)
+print(f'{p1=}')
+print(f'{p2=}')
