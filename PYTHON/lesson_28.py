b2db823a48d7d8ef7efa8478f29a275266cefb83
v <v@mail.ru>
2024-03-20 18:07:54 +0500

    lesson_28: selenium booktuscrape парсер

--- PYTHON/lesson_28.py
+++ PYTHON/lesson_28.py
@@ -4,34 +4,8 @@ Lesson 28
 ООП
 Selenium webdriver
 # Установка selenium - pip install selenium
-"""
-import time
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-# Настройки
-options = webdriver.ChromeOptions()
-
-SETTINGS = [
-    "--start-maximized",  # Открыть браузер на весь экран
-     # "--new-headless" # Открыть браузер в фоне
-    "--incognito",  # Включить режим инкогнито
-    # "--window-size=1920,1080", # Размеры окна
-]
-
-for params in SETTINGS:
-    options.add_argument(params)
-
-# Создаем экземпляр драйвера
-driver = webdriver.Chrome(options=options)
-
-# Переходим на сайт
-driver.get("https://www.google.com")
-# Скриншот страницы
-driver.save_screenshot("google.png")
-time.sleep(2)
-
+# NoSuchElementException - исключение, которое возникает, когда элемент не найден
 
 
 # Поиск элементв на странице с помощью объекта By
@@ -47,18 +21,17 @@ time.sleep(2)
 # By.PARTIAL_LINK_TEXT
 # By.XPATH
 # By.CSS_SELECTOR
+"""
+import json
+import time
+from pprint import pprint
+from typing import List
 
-# Ищем по CSS texarea аттрибут title="Поиск"
-search_input = driver.find_element(By.CSS_SELECTOR, 'textarea[title="Поиск"]')
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
-# Ввожу сообщение
-message = "Привет группа!"
-search_input.send_keys(message)
-
-# Нажемем enter через клавиатуру (KEYS)
-search_input.submit()
-# search_input.send_keys(webdriver.common.keys.Keys.ENTER)
-time.sleep(5)
 
 def get_driver(driver_options: list = None) -> webdriver.Chrome:
     """
@@ -70,6 +43,81 @@ def get_driver(driver_options: list = None) -> webdriver.Chrome:
     if driver_options:
         for option in driver_options:
             options.add_argument(option)
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options)
     driver.implicitly_wait(10)  # Устанавливаем неявное ожидание 10 секунд
     return driver
+
+
+SETTINGS = [
+    "--start-maximized",  # Открыть браузер на весь экран
+    # "--new-headless" # Открыть браузер в фоне
+    "--incognito",  # Включить режим инкогнито
+    # "--window-size=1920,1080", # Размеры окна
+]
+
+
+
+# Создаем экземпляр драйвера
+driver = get_driver(SETTINGS)
+
+# Переходим на сайт
+driver.get("http://books.toscrape.com/index.html")
+
+# Ищем все товарные карточки на странице
+# article class product_pod
+# books : List[WebElement] = driver.find_elements(By.CLASS_NAME, "product_pod")
+
+book_marks = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    "Five": 5
+}
+
+result = []
+while True:
+    # Нашли все книги на странице
+    books = driver.find_elements(By.CLASS_NAME, "product_pod")
+
+    # Спарсили информацию о книгах на странице
+    for book in books:
+        # h3 > a[title] - содержимое title
+        title = book.find_element(By.CSS_SELECTOR, "h3 > a").get_attribute("title")
+        text_mark = book.find_element(By.CSS_SELECTOR, "p[class^='star-rating']").get_attribute("class").split()[1]
+        mark = book_marks.get(text_mark)
+        # Цена по имени класса price_color
+        price_str = book.find_element(By.CLASS_NAME, "price_color").text
+        price = float(price_str.lstrip('£'))
+        # Найдем наличие в продаже. Div product_price в нем второй ребенок p - содержит текст
+        in_stock_str = book.find_element(By.CSS_SELECTOR, "div.product_price").find_elements(By.TAG_NAME, "p")[1].text
+        in_stock = True if in_stock_str == "In stock" else False
+        # img_url - src тега img
+        row_img_url = book.find_element(By.TAG_NAME, "img").get_attribute("src")
+        img_url = f'{row_img_url.lstrip('..')}'
+
+
+        result.append({
+            "title": title,
+            "mark": mark,
+            "price": price,
+            "in_stock": in_stock,
+            "img_url": img_url
+        })
+
+
+
+    # Пробуем найти a с текстом Next
+    try:
+        next_page = driver.find_element(By.LINK_TEXT, "next")
+        next_page.click()
+        time.sleep(1)
+
+    except NoSuchElementException:
+        break
+
+# Сохраняем результат в файл json encoding='utf-8', ensure_ascii=False indent=4
+with open('books.json', 'w', encoding='utf-8') as file:
+    json.dump(result, file, ensure_ascii=False, indent=4)
+
+pprint(result, sort_dicts=False)
