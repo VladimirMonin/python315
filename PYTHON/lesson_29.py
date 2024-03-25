5916995b7060b531c8c4ddd5dd2ecd2fe423d58f
v <v@mail.ru>
2024-03-25 18:45:27 +0500

    lesson_29: решение практики

--- PYTHON/lesson_29.py
+++ PYTHON/lesson_29.py
@@ -5,8 +5,8 @@ Lesosn 29
 @dataclass(order=True) - для сравнения
 Метаинформация в полях (для сериализации и других целей)
 Наследование датаклассов
-Метаклассы - как концепция
 Декорация классов
+Метаклассы - как концепция
 """
 
 from dataclasses import dataclass, field, fields, asdict
@@ -81,3 +81,37 @@ price (цена, вещественное число).
     Демонстрируйте сравнение товаров по цене.
     Выведите товары в отсортированном порядке по цене.
 """
+
+
+@dataclass(order=True)
+class Product:
+    name: str = field(compare=False)
+    category: str = field(compare=False)
+    price: float
+
+@dataclass(order=True)
+class Notebook(Product):
+    screen_size: float = field(compare=False)
+    cpu: str = field(compare=False)
+    ram: float = field(compare=False)
+
+
+@dataclass(order=True)
+class Phone(Product):
+    brand: str = field(compare=False)
+    os: str = field(compare=False)
+    memory: float = field(compare=False)
+
+
+n1 = Notebook("Notebook 1", "Notebooks", 1000, 15.6, "Intel Core i5", 8)
+n2 = Notebook("Notebook 2", "Notebooks", 1500, 15.6, "Intel Core i7", 16)
+n3 = Notebook("Notebook 3", "Notebooks", 1200, 15.6, "Intel Core i5", 8)
+
+p1 = Phone("Phone 1", "Phones", 500, "Samsung", "Android", 128)
+
+products = [n1, n2, n3]
+print(sorted(products))
+
+products2 = [n1, n2, n3, p1]
+# print(sorted(products2)) # TypeError: '<' not supported between instances of 'Notebook' and 'Phone'
+print(sorted(products2, key=lambda product: product.price))
