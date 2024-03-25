ea7017d8b1bec01aba7be0b776d28e18873f346a
v <v@mail.ru>
2024-03-25 17:28:51 +0500

    lesson_29: датаклассы: базовое наследование

--- PYTHON/lesson_29.py
+++ PYTHON/lesson_29.py
@@ -18,7 +18,6 @@ class Person:
     name: str = field(compare=False, metadata={"description": "Имя"})
     age: int = field(metadata={"description": "Возраст"})
     city: str = field(compare=False, metadata={"description": "Город"})
-    hobbies: list[str] = field(default_factory=list, compare=False, metadata={"description": "Хобби"})
 
     def __str__(self):
         """
@@ -29,31 +28,15 @@ class Person:
         meta_str = ", ".join([f"{f.metadata['description']}: {getattr(self, f.name)}" for f in fields(self)])
         return meta_str
 
-p1 = Person("Вася", 25, "Москва", ["программирование", "JS"])
-p2 = Person("Маша", 25, "Москва", ["программирование", "Python"])
 
-print(p1 == p2)
+@dataclass(order=True)
+class Employee(Person):
+    position: str = field(metadata={"description": "Должность"})
+    salary: int = field(metadata={"description": "Зарплата"})
+
+
+p1 = Employee("Вася", 25, "Москва", "Программист", 100_000)
+p2 = Employee("Маша", 30, "Москва", "Программист", 100_000)
+
 print(p1)
-
-# Получим метаинформацию о полях из экземпляра p2
-# Получили список полей, представленных в виде экземпляров Field
-print(fields(p2))
-# Получим из fields метаинформацию,
-print(fields(p2)[0].metadata)
-
-[print(s.metadata) for s in fields(p2)]
-
-# Получим данные из fields
-result = [f"{f.metadata['description']}: {getattr(p1, f.name)}" for f in fields(p1)]
-print(result)
-
-# Сделаем это в виде цикла
-result = []
-
-for f in fields(p1):
-    meta_description = f.metadata['description']
-    value = getattr(p1, f.name)
-    result.append(f"{meta_description}: {value}")
-
-print(result)
-
+print(p2)
