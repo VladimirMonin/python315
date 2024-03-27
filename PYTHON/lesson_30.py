d228242d6256600e9e45ebe6b6f0024b1d27c21a
v <v@mail.ru>
2024-03-27 18:49:06 +0500

    lesson_30: pickle

--- PYTHON/lesson_30.py
+++ PYTHON/lesson_30.py
@@ -5,58 +5,36 @@ Class decorators
 Meta classes
 Вложенные классы
 Итераторы
-Сериализация
+Теория сериализации и десериализации
+Библиотека pickle
 """
 
+import pickle
 from dataclasses import dataclass
 
 @dataclass
 class Person:
     name: str
     age: int
-
-    def __str__(self):
-        return f'Экземпляр класса Person: {self.name}, {self.age}'
+    city: str
 
 
+# data = {"key": "value"}
+data = Person("Ivan", 30, "Moscow")
 
-# Класс итератор, который примет назвнаие txt файла и будет возвращать по одной строке из файла
-# Преобразовывая это в экземпляр класса Person
-    
-class PersonTXTIterator:
-    def __init__(self, file_name: str, encoding: str = "utf-8"):
-        self.file_name = file_name
-        self.file = open(file_name, "r", encoding="utf-8")
 
-    def __iter__(self):
-        return self
+serialized_data = pickle.dumps(data)
+print(serialized_data)
 
-    def __next__(self) -> Person:
-        line = self.file.readline().strip()
-        if not line:
-            self.file.close()
-            raise StopIteration
-        # Используем eval для преобразования строки в Person
-        result = None
-        try:
-           result: Person = eval(line)
-        except Exception as e:
-            print(e)
-        
-        return result
-    
 
-"""
-Датасет для теста (содержимое файла)
-Person(name='Тимур', age=25)
-Person(name='Павел', age=30)
-Person(name='Оксана', age=22)
-Person(name='Алексей', age=35)
-Person(name='Виталий', age=27)
-"""
+FILE = r"data\data.pickle"
 
-FILE = r"data\persons.txt"
-person_iterator = PersonTXTIterator(FILE)
 
-for person in person_iterator:
-    print(person)
+with open(FILE, "wb") as file:
+    pickle.dump(data, file)
+
+
+with open(FILE, "rb") as file:
+    data = pickle.load(file)
+    print(data)
+    print(type(data))
