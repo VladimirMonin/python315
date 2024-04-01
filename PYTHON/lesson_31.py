0e1d1b9cda55f0009c11e79a56c8f9c3715b26e9
v <v@mail.ru>
2024-04-01 19:03:30 +0500

    lesson_31: встроенные валидаторы marshmallow

--- PYTHON/lesson_31.py
+++ PYTHON/lesson_31.py
@@ -15,7 +15,7 @@ PostLoad - обработка данных после загрузки
 
 from dataclasses import dataclass, field
 from typing import Union, Dict
-from marshmallow import Schema, fields, post_load, ValidationError, pre_load
+from marshmallow import Schema, fields, post_load, ValidationError, pre_load, validate
 from data import cities
 
 # print(cities.cities_list)
@@ -32,7 +32,18 @@ cities_list = [
         "population": 14816,
         "subject": "Хакасия",
         "email": "a@s.ru"
-    }
+    },
+    {
+        "coords": {
+            "lat": "53.71667",
+            "lon": "91.41667"
+        },
+        "district": "Сибирский",
+        "name": "Абакан",
+        "population": -165214,
+        "subject": "Хакасия",
+        "email": "abakan@a.ru"
+    },
 ]
 
 
@@ -49,12 +60,12 @@ class City:
 
 class CitySchema(Schema):
     district = fields.Str()
-    name = fields.Str()
-    population = fields.Int()
+    name = fields.Str(validate=validate.Length(min=2, max=50))
+    population = fields.Int(validate=validate.Range(min=0, max=50_000_000))
     subject = fields.Str()
     coords = fields.Dict()
-    lat = fields.Float(required=False)
-    lon = fields.Float(required=False)
+    lat = fields.Float(required=False, validate=validate.Range(min=-90, max=90))
+    lon = fields.Float(required=False, validate=validate.Range(min=-90, max=90))
     email = fields.Email()
 
     @post_load
@@ -70,8 +81,8 @@ class CitySchema(Schema):
 
 
 city_schema = CitySchema()
-city = city_schema.load(cities_list[0])
+city = city_schema.load(many=True, data=cities_list)
 print(city)
 # Преобразование данных в словарь
-city_dict = city_schema.dump(city)
+city_dict = city_schema.dump(many=True, obj=city)
 print(city_dict)
