95bdfd82bcb1021b72d0a6125d69a40cc2f83c1b
v <v@mail.ru>
2024-04-01 18:49:29 +0500

    lesson_31: базовый синтаксис marshmallow

--- PYTHON/lesson_31.py
+++ PYTHON/lesson_31.py
@@ -4,33 +4,36 @@ Lesson 31
 
 Концепция валидации данных
 Ручной класс сериализатор и валидатор
-Валидация с помощью marshmallow
 Установка marshmallow
+pip install marshmallow marshmallow-dataclass
+Валидация с помощью marshmallow
 Концепция схемы и поля
+PostLoad - обработка данных после загрузки
 Настройки полей
-PostLoad
+
 """
 
 from dataclasses import dataclass, field
 from typing import Union, Dict
-
+from marshmallow import Schema, fields, post_load, ValidationError, pre_load
 from data import cities
 
 # print(cities.cities_list)
 
-"""
+
 cities_list = [
-  {
-    "coords": {
-      "lat": "52.65",
-      "lon": "90.08333"
-    },
-    "district": "Сибирский",
-    "name": "Абаза",
-    "population": 14816,
-    "subject": "Хакасия"
-  },
-"""
+    {
+        "coords": {
+            "lat": "52.65",
+            "lon": "90.08333"
+        },
+        "district": "Сибирский",
+        "name": "Абаза",
+        "population": 14816,
+        "subject": "Хакасия",
+        "email": "a@s.ru"
+    }
+]
 
 
 @dataclass
@@ -41,72 +44,34 @@ class City:
     subject: str
     lat: float
     lon: float
+    email: str
 
 
-class CityValidator:
-    def __init__(self):
-        self.city: dict = {}
+class CitySchema(Schema):
+    district = fields.Str()
+    name = fields.Str()
+    population = fields.Int()
+    subject = fields.Str()
+    coords = fields.Dict()
+    lat = fields.Float(required=False)
+    lon = fields.Float(required=False)
+    email = fields.Email()
 
-    def validate(self, city: dict):
-        self.city = city
-        if not isinstance(self.city["name"], str):
-            raise ValueError("Название города должно быть строкой")
-        if not isinstance(self.city["population"], int):
-            raise ValueError("Население должно быть числом")
-        if not isinstance(self.city["district"], str):
-            raise ValueError("Район должен быть строкой")
-        if not isinstance(self.city["subject"], str):
-            raise ValueError("Субъект должен быть строкой")
-        if not isinstance(self.city["coords"], dict):
-            raise ValueError("Координаты должны быть словарем")
-        lat = self.city["coords"]["lat"]
-        try:
-            float(lat)
-        except ValueError:
-            raise ValueError(f"Широта должна быть числом {lat}")
-        lon = self.city["coords"]["lon"]
-        try:
-            float(lon)
-        except ValueError:
-            raise ValueError(f"Долгота должна быть числом {lon}")
-        return self.city
+    @post_load
+    def make_city(self, data, **kwargs):
+        coords = data.pop('coords')
+        data['lat'] = coords['lat']
+        data['lon'] = coords['lon']
+
+        return City(**data)
 
 
-class CitySerializer:
-    def __init__(self):
-        self.__city: Union[None | Dict | City] = None
-        self.__city_validator = CityValidator()
-
-    def __city_validate(self, city: dict):
-        return self.__city_validator.validate(city)
-
-    def serialize(self, city: City):
-        self.__city = city
-        return {
-            "name": self.__city.name,
-            "population": self.__city.population,
-            "district": self.__city.district,
-            "subject": self.__city.subject,
-            "coords": {
-                "lat": self.__city.lat,
-                "lon": self.__city.lon
-            }
-        }
-
-    def deserialize(self, data):
-        self.__city = self.__city_validate(data)
-
-        return City(
-            name=self.__city["name"],
-            population=self.__city["population"],
-            district=self.__city["district"],
-            subject=self.__city["subject"],
-            lat=float(self.__city["coords"]["lat"]),
-            lon=float(self.__city["coords"]["lon"])
-        )
 
 
-city_serializer = CitySerializer()
 
-cities = [city_serializer.deserialize(city) for city in cities.cities_list]
-print(cities)
+city_schema = CitySchema()
+city = city_schema.load(cities_list[0])
+print(city)
+# Преобразование данных в словарь
+city_dict = city_schema.dump(city)
+print(city_dict)
