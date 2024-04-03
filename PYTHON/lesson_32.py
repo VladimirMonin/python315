aa6281aa449df8d7b9d7b01ae880448beeb3e007
v <v@mail.ru>
2024-04-03 18:34:55 +0500

    lesson_32: расширение схемы marshmallow_dataclass своей схемой

--- PYTHON/lesson_32.py
+++ PYTHON/lesson_32.py
@@ -7,11 +7,14 @@ Lesson 32
 - Схема с прелоад и постлоад
 - Схема с вложенными схемами
 - Схема на основе датакласса (marshmallow_dataclass)
+- Дополнительные парметры через метаданные в датаклассе (marshmallow_dataclass)
+- Расширение своей схемой схемы созданной на основе датакласса
 """
 
 from dataclasses import dataclass, field
 from marshmallow import Schema, fields, pre_load, post_load
 import marshmallow_dataclass
+from marshmallow.validate import Range
 
 
 @dataclass
@@ -19,7 +22,7 @@ class City:
     name: str
     district: str
     subject: str
-    population: int
+    population: int = field(metadata={'validate': Range(min=1, max=1000000)})
     email: str = field(default="", metadata={"load_default": "",
                                              "load_only": True})
     coords: dict = field(default_factory=dict)
@@ -33,7 +36,7 @@ cities_list = [
     },
         "district": "Сибирский",
         "name": "Абаза",
-        "population": -14816,
+        "population": 14816,
         "subject": "Хакасия"
     },
 
@@ -51,13 +54,21 @@ cities_list = [
 # Создание схемы на основе датакласса
 CitySchema = marshmallow_dataclass.class_schema(City)
 
-# Создание экземпляра схемы
-city_schema = CitySchema(many=True)
+# Расширяем своей схемой, схему созданную на основе датакласса
 
-# Десериализация данных
+class CoordsSchema(Schema):
+    lat = fields.Float()
+    lon = fields.Float()
+
+class CitySchemaExtended(CitySchema):
+    coords = fields.Nested(CoordsSchema, required=True)
+
+
+# Десериализация
+city_schema = CitySchemaExtended(many=True)
 cities = city_schema.load(cities_list)
 print(cities)
 
-# Сериализация данных
-data = city_schema.dump(cities)
-print(data)
+# Сериализация
+result = city_schema.dump(cities)
+print(result)
