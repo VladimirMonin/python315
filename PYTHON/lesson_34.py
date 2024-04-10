21f9c61fcd7f459a7c74b97e4e2e5a6079d8132a
v <v@mail.ru>
2024-04-10 19:16:54 +0500

    lesson_34: абстрактная фабрика

--- PYTHON/lesson_34.py
+++ PYTHON/lesson_34.py
@@ -4,112 +4,81 @@ Lesson 34: Порождающие паттерны
 - Singleton - одиночка
 - Prototype - прототип
 - Builder - строитель
+- Abstract Factory - абстрактная фабрика
 """
 
-# Builder - строитель
-# Позволяет создавать сложные объекты пошагово
+# Abstract Factory - абстрактная фабрика
+# Паттерн Abstract Factory предоставляет интерфейс для создания семейств взаимосвязанных или взаимозависимых объектов,
+# не специфицируя их конкретных классов.
 
-# Пример 1 BurgerBuilder
 from abc import ABC, abstractmethod
 
-class Burger:
+class AbstractProductA(ABC):
+    """
+    Абстрактный продукт A. Определяет интерфейс для продуктов семейства A.
+    """
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+class ConcreteProductA1(AbstractProductA):
+    """
+    Конкретный продукт A1. Реализует интерфейс продукта A.
+    Создается фабрикой 1.
+    """
+    def operation(self) -> str:
+        return "Результат работы ConcreteProductA1"
+
+class ConcreteProductA2(AbstractProductA):
+    """
+    Конкретный продукт A2. Реализует интерфейс продукта A.
+    Создается фабрикой 2.
+    """
+    def operation(self) -> str:
+        return "Результат работы ConcreteProductA2"
+
+class AbstractFactory(ABC):
+    """Абстрактная фабрика"""
+    @abstractmethod
+    def create_product_a(self) -> AbstractProductA:
+        pass
+
+class ConcreteFactory1(AbstractFactory):
+    """
+    Конкретная фабрика 1. Делает продукты сеймейства 1.
+    """
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA1()
+
+class ConcreteFactory2(AbstractFactory):
+    """
+    Конкретная фабрика 2. Делает продукты сеймейства 2.
+    """
+    def create_product_a(self) -> AbstractProductA:
+        return ConcreteProductA2()
+
+
+# Facade
+# Паттерн Facade предоставляет простой интерфейс к сложной системе классов, библиотеке или фреймворку.
+    
+class Facade:
     def __init__(self):
-        self.ingredients: List[str] = []
+        self.factory: AbstractFactory | None = None
 
-    def add_ingredient(self, ingredient: str) -> None:
-        self.ingredients.append(ingredient)
-
-    def get_margeting_str(self) -> str:
-        """Метод определяется в конкретных строителях"""
-        pass
-
-
-class AbstractBurgerBuilder(ABC):
-    @abstractmethod
-    def prepare_bun(self) -> None:
-        pass
-
-    @abstractmethod
-    def insert_patty(self) -> None:
-        pass
-
-    @abstractmethod
-    def add_condiments(self) -> None:
-        pass
-
-    def create_burger(self) -> None:
-        self.burger = Burger()
-
-
-    def get_burger(self) -> Burger:
-        return self.burger
-    
-
-class FishBurgerBuilder(AbstractBurgerBuilder):
-    def prepare_bun(self) -> None:
-        self.burger.add_ingredient('Булочка для рыбургера')
-
-    def insert_patty(self) -> None:
-        self.burger.add_ingredient('Рыбный котлет')
-
-    def add_condiments(self) -> None:
-        self.burger.add_ingredient('Соус тар-тар')
-
-    def get_margeting_str(self) -> str:
-        return f'Самый вкусный рыбургер в мире! Содержит {", ".join(self.burger.ingredients)}'
-
-    def create_burger(self) -> None:
-        super().create_burger()
-        self.prepare_bun()
-        self.insert_patty()
-        self.add_condiments()
-
-
-
-
-class BurgerBuilder(AbstractBurgerBuilder):
-    def prepare_bun(self) -> None:
-        self.burger.add_ingredient('Булочка для бургера')
-
-    def insert_patty(self) -> None:
-        self.burger.add_ingredient('Говяжий котлет')
-
-    def add_condiments(self) -> None:
-        self.burger.add_ingredient('Кетчуп')
-
-    def add_cheese(self) -> None:
-        self.burger.add_ingredient('Сыр')
-
-    def get_margeting_str(self) -> str:
-        return f'Самый фукусный бургер в мире! Содержит {", ".join(self.burger.ingredients)}'
-    
-    def create_burger(self) -> None:
-        super().create_burger()
-        self.prepare_bun()
-        self.insert_patty()
-        self.add_condiments()
-        self.add_cheese()
-        self.burger.get_margeting_str = self.get_margeting_str
-
-
-    
-class BuregerDirector:
-    def __init__(self) -> None:
-        self.builder: AbstractBurgerBuilder | None = None
-
-    def __call__(self, burger_type: str) -> Burger:
-        if burger_type == 'burger':
-            self.builder = BurgerBuilder()
-        elif burger_type == 'fishburger':
-            self.builder = FishBurgerBuilder()
+    def __call__(self):
+        user_input = input("Введите 1 или 2: ")
+        if user_input == "1":
+            self.factory = ConcreteFactory1()
+        elif user_input == "2":
+            self.factory = ConcreteFactory2()
         else:
-            raise ValueError('Неверный тип бургера')
+            raise ValueError("Неверный ввод")
+        
+        if self.factory:
+            product = self.factory.create_product_a()
+            print(product.operation())
 
-        self.builder.create_burger()
-        return self.builder.get_burger()
 
-
-
-director = BuregerDirector()
-b_burger = director('burger')
-print(b_burger.get_margeting_str())
+if __name__ == "__main__":
+    facade = Facade()
+    facade()
