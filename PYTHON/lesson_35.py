7ba5b4d273dc99105c1086bb286bf70fc8f55b18
v <v@mail.ru>
2024-04-15 18:33:49 +0500

    lesson_35: прокси документ

--- PYTHON/lesson_35.py
+++ PYTHON/lesson_35.py
@@ -5,86 +5,52 @@ Lesson 35: Структурные паттерны
 - Abstract Method - абстрактный метод
 - Facade - фасад
 - Adapter - адаптер
+- Decorator - декоратор - повторили
+- Proxy - заместитель
 """
 from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
 
-# Adapter - адаптер, паттерн, который позволяет объектам с несовместимыми интерфейсами работать вместе.
+
+# Proxy - заместитель
+# Паттерн Proxy предоставляет объект-заместитель или заполнитель для другого объекта, чтобы контролировать доступ к нему.
 
 """
-Напишем 2 JSON файла с погодными данными, с разной структурой
-Напишем датакласс Weather, который будет принимать данные из JSON файла
-Напишем адааптер, который будет принимать данные из JSON файла и преобразовывать их в объект Weather
+Напишем простой пример с использованием паттерна Proxy.
+Асбтрактный класс Документ, который имеет метод открыть и закрыть документ.
+А так же класс ProxyDocument, который является заместителем для класса Document.
 """
 
-@dataclass
-class Weather:
-    city: str
-    temperature: int
-    wind_speed: int
-    pressure: int
-    humidity: int
-
-    def __str__(self):
-        return f"{self.city}: {self.temperature}°C, {self.wind_speed} м/с, {self.pressure} мм.рт.ст., {self.humidity}%"
-
-
-weather_data_1 = {
-    "city": "Moscow",
-    "temperature": 15,
-    "wind_speed": 5,
-    "pressure": 760,
-    "humidity": 70
-}
-
-weather_data_2 = {
-    "main_info" : {
-        "city": "Moscow",
-        "temperature": 15
-    },
-    "wind_info": {
-        "speed": 5
-    },
-    'pressure_info': {
-        "pressure": 760
-    },
-    }
-
-# Адаптер
-class AbstractAdapter(ABC):
-    
-    def __init__(self, weather_data):
-        self.weather_data = weather_data
-    
-    def load_data(self, weather_data):
-        self.weather_data = weather_data
+class Document(ABC):
+    @abstractmethod
+    def open(self):
+        pass
 
     @abstractmethod
-    def adapt_data(self):
+    def close(self):
         pass
 
 
-class WeatherAdapter1(AbstractAdapter):
-    def adapt_data(self):
-        return Weather(
-            city=self.weather_data["city"],
-            temperature=self.weather_data["temperature"],
-            wind_speed=self.weather_data["wind_speed"],
-            pressure=self.weather_data["pressure"],
-            humidity=self.weather_data["humidity"]
-        )
-    
+@dataclass
+class SecretDocument(Document):
+    secret: str
 
-class WeatherAdapter2(AbstractAdapter):
-    def adapt_data(self):
-        return Weather(
-            city=self.weather_data["main_info"]["city"],
-            temperature=self.weather_data["main_info"]["temperature"],
-            wind_speed=self.weather_data["wind_info"]["speed"],
-            pressure=self.weather_data["pressure_info"]["pressure"],
-            humidity=0
-        )
-    
+    def open(self):
+        print("Открываем секретный документ")
 
-weather1 = WeatherAdapter1(weather_data_1).adapt_data()
-weather2 = WeatherAdapter2(weather_data_2).adapt_data()
+    def close(self):
+        print("Закрываем секретный документ")
+
+
+@dataclass
+class ProxyDocument(Document):
+    def __post_init__(self):
+        self.secret_document = SecretDocument(secret="Секретный текст")
+    
+    def open(self):
+        password = input("Введите пароль: ")
+        if password == "123":
+            self.secret_document.open()
+
+    def close(self):
+        self.secret_document.close()
