b899844ec86030c8cf914461e8f19014ee5d8d47
v <v@mail.ru>
2024-04-15 17:36:58 +0500

    lesson_35: пример реального фасада с браузерами

--- PYTHON/lesson_35.py
+++ PYTHON/lesson_35.py
@@ -6,35 +6,109 @@ Lesson 35: Структурные паттерны
 - Facade - фасад
 """
 
-# Facade - фасад
-# Паттерн Facade предоставляет унифицированный интерфейс к группе интерфейсов в подсистеме.
+import time
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from pprint import pprint
+from typing import List
 
-# Простой пример
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
-# Сложная система (подсистемы)
-class SubsystemOne:
-    def operation(self):
-        return "Подсистема 1: Поехали!"
 
-class SubsystemTwo:
-    def operation(self):
-        return "Подсистема 2: Поехали!"
+@dataclass
+class Book:
+    """
+    Дата-класс для хранения информации о книге
+    """
+    title: str
+    price: float
+    rating: int
+    img_url: str
 
-# Фасад
-class Facade:
+
+# Абстрактный класс AbstractBrowser и конкретные классы ChromeBrowser и FirefoxBrowser
+
+class AbstractBrowser(ABC):
+    """
+    Абстрактный класс для создания экземпляра браузера
+    """
+    implicit_wait = 10
+
+    @abstractmethod
+    def get_driver(self, driver_options: list = None):
+        pass
+
+    @abstractmethod
+    def get_options(self, driver_options: list = None):
+        pass
+
+
+class ChromeBrowser(AbstractBrowser):
+    """
+    Конкретный класс для создания экземпляра браузера Chrome
+    """
     def __init__(self):
-        self._subsystem_one = SubsystemOne()
-        self._subsystem_two = SubsystemTwo()
+        self.driver = None
 
-    def operation(self):
-        user_choise = input("Какую подсистему запустить? 1 или 2: ")
-        if user_choise == "1":
-            print("Запускаем подсистему 1")
-            return self._subsystem_one.operation()
-        elif user_choise == "2":
-            print("Запускаем подсистему 2")
-            return self._subsystem_two.operation()
+    def get_options(self, driver_options: list = None):
+        options = webdriver.ChromeOptions()
+        if driver_options:
+            for option in driver_options:
+                options.add_argument(option)
+        return options
 
-# Клиентский код
-facade = Facade()
-print(facade.operation())
+    def get_driver(self, driver_options: list = None):
+        options = self.get_options(driver_options)
+        driver = webdriver.Chrome(options)
+        driver.implicitly_wait(self.implicit_wait)
+        self.driver = driver
+
+
+class FirefoxBrowser(AbstractBrowser):
+    """
+    Конкретный класс для создания экземпляра браузера Firefox
+    """
+    def __init__(self):
+        self.driver = None
+
+    def get_options(self, driver_options: list = None):
+        options = webdriver.FirefoxOptions()
+        if driver_options:
+            for option in driver_options:
+                options.add_argument(option)
+        return options
+
+    def get_driver(self, driver_options: list = None):
+        options = self.get_options(driver_options)
+        driver = webdriver.Firefox(options)
+        driver.implicitly_wait(self.implicit_wait)
+        self.driver = driver
+
+
+# Facade - управляющий класс, который спросит, какой браузер я хочу
+
+class ParserFacede:
+    def __init__(self):
+        self.browser = None | AbstractBrowser
+
+    def __call__(self):
+        user_choise = input("Какой браузер использовать? Chrome или Firefox: ")
+        if user_choise.lower() == "chrome":
+            self.browser = ChromeBrowser()
+        elif user_choise.lower() == "firefox":
+            self.browser = FirefoxBrowser()
+        else:
+            print("Некорректный ввод")
+            return
+        
+        self.browser.get_driver()
+        self.browser.driver.get("http://books.toscrape.com/")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    facade = ParserFacede()
+    facade()
