902a42067b0425bbfc360e23584a457c74ff8435
v <v@mail.ru>
2024-04-17 17:56:16 +0500

    lesson_36: имитация работы датчиков

--- PYTHON/lesson_36.py
+++ PYTHON/lesson_36.py
@@ -9,6 +9,7 @@ Lesson 36: Поведенческие паттерны
 # Напишем пример с датчиками умного дома и их наблюдателями.
 
 from abc import ABC, abstractmethod
+import time
 
 class Sensor(ABC):
     """
@@ -28,25 +29,29 @@ class Sensor(ABC):
         for observer in self.observers:
             observer.update(self.name, value)
 
-    @abstractmethod
+    
     def get_value(self):
-        pass
+        threshold = 50
+        for i in range(threshold):
+            time.sleep(0.1)
+            if i == threshold - 1:
+                self.notify_observers(i)
 
 
 class TemperatureSensor(Sensor):
     """
     Класс датчика температуры
     """
-    def get_value(self):
-        return 25
+    pass
+
+        
     
 
 class HumiditySensor(Sensor):
     """
     Класс датчика влажности
     """
-    def get_value(self):
-        return 50
+    pass
     
 class AbstractObserver(ABC):
     """
@@ -73,23 +78,24 @@ class FileObserver(AbstractObserver):
             file.write(f"Датчик {sensor_name} сообщает значение: {value} в {self.__class__.__name__}\n")
 
 
-# Тесты
+# Четыре датчика
 t1 = TemperatureSensor("Температурный датчик №1")
 t2 = TemperatureSensor("Температурный датчик №2")
 h1 = HumiditySensor("Датчик влажности №1")
 h2 = HumiditySensor("Датчик влажности №2")
 
+# Два наблюдателя
 c1 = ConsoleObserver()
 f1 = FileObserver()
 
 devises = [t1, t2, h1, h2]
 observers = [c1, f1]
 
-# Подписываем наблюдателей на датчики
+# Подписываем все датчики на обоих наблюдателей
 for device in devises:
     for observer in observers:
         device.add_observer(observer)
 
-# Получаем значения датчиков
+# Запускаем "работу" датчиков
 for device in devises:
-    device.notify_observers(device.get_value())
+    device.get_value()
