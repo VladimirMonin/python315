934ae7f8b4afd216cf493b5ebb48f46c293b2a76
v <v@mail.ru>
2024-04-17 17:35:52 +0500

    lesson_36: сложный пример

--- PYTHON/lesson_36.py
+++ PYTHON/lesson_36.py
@@ -9,61 +9,119 @@ Lesson 36: Поведенческие паттерны
 
 
 from abc import ABC, abstractmethod
+import time
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from pprint import pprint
+from typing import List
 
-class AbstractHelloStrategy(ABC):
-    
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+
+class AbstractBrowserStrategy(ABC):
+    """
+    Абстрактный класс стратегии браузера
+    """
+
+    def __init__(self, settings: List[str]):
+        self.settings = settings
+        self.implicity_wait = 10
+
     @abstractmethod
-    def hello(self):
+    def get_options_object(self):
+        pass
+
+    @abstractmethod
+    def get_driver(self):
         pass
 
 
-class FriendlyHelloStrategy(AbstractHelloStrategy):
-    def hello(self):
-        print("Привет! Как дела?")
+class ChromeBrowserStrategy(AbstractBrowserStrategy):
+    """
+    Конкретная стратегия для браузера Chrome
+    """
 
+    def get_options_object(self):
+        """
+        Получение объекта настроек для Chrome
+        """
+        options = webdriver.ChromeOptions()
+        if self.settings:
+            for option in self.settings:
+                options.add_argument(option)
+        return options
 
-class BuisnessHelloStrategy(AbstractHelloStrategy):
-    def hello(self):
-        print("Здравствуйте! Какие новости?")
-
-
-class FamilyHelloStrategy(AbstractHelloStrategy):
-    def hello(self):
-        print("Здорово! Как день прошел?")
-
-
-class HelloContext:
-    def __init__(self, strategy: AbstractHelloStrategy):
-        self.strategy = strategy
-
-    def greet(self):
-        self.strategy.hello()
-
-    def set_strategy(self, strategy: AbstractHelloStrategy):
-        self.strategy = strategy
-
-
-class HelloFacade:
-    def __init__(self):
-        self.friendly : FriendlyHelloStrategy = FriendlyHelloStrategy()
-        self.buisness : BuisnessHelloStrategy = BuisnessHelloStrategy()
-        self.family : FamilyHelloStrategy = FamilyHelloStrategy()
-        self.context : HelloContext = HelloContext(self.friendly)
-
+    def get_driver(self):
+        """
+        Получение драйвера для Chrome
+        """
+        options = self.get_options_object()
+        driver = webdriver.Chrome(options)
+        driver.implicitly_wait(self.implicity_wait)
+        return driver
     
-    def greet(self):
-        user_input = input("Какой тип приветствия вы хотите использовать? (1 - Дружеское, 2 - Деловое, 3 - Семейное): ")
-        if user_input == "1":
-            self.context.set_strategy(self.friendly)
-        elif user_input == "2":
-            self.context.set_strategy(self.buisness)
-        elif user_input == "3":
-            self.context.set_strategy(self.family)
-        else:
-            print("Неверный ввод")
-        
-        self.context.greet()
-        
 
-hello = HelloFacade()
-hello.greet()
+class FirefoxBrowserStrategy(AbstractBrowserStrategy):
+    """
+    Конкретная стратегия для браузера Firefox
+    """
+
+    def get_options_object(self):
+        """
+        Получение объекта настроек для Firefox
+        """
+        options = webdriver.FirefoxOptions()
+        if self.settings:
+            for option in self.settings:
+                options.add_argument(option)
+        return options
+
+    def get_driver(self):
+        """
+        Получение драйвера для Firefox
+        """
+        options = self.get_options_object()
+        driver = webdriver.Firefox(options)
+        driver.implicitly_wait(self.implicity_wait)
+        return driver
+    
+
+
+class Browser:
+    """
+    Класс - контекст для работы с одним из стратегий-браузеров
+    """
+    
+    def __init__(self):
+        self.strategy = FirefoxBrowserStrategy([])
+
+    def set_strategy(self, strategy: AbstractBrowserStrategy):
+        """
+        Установка стратегии
+        """
+        self.strategy = strategy
+
+    def get_driver(self):
+        """
+        Получение драйвера
+        """
+        return self.strategy.get_driver()
+    
+
+def main():
+    browser = Browser() # Экземпляр контекста
+    browser.set_strategy(ChromeBrowserStrategy(["--incognito"])) # Установка стратегии
+    driver = browser.get_driver() # Заставил контекст выполнить действие
+    
+    # Использую результат действия
+    driver.get("https://google.com")
+    time.sleep(5)
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
