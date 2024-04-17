4ea7c460c29f02039edb4edfed13eb47b2c43cc3
v <v@mail.ru>
2024-04-17 19:22:03 +0500

    lesson_36: Состояние (не работает)

--- PYTHON/lesson_36.py
+++ PYTHON/lesson_36.py
@@ -6,51 +6,110 @@ Lesson 36: Поведенческие паттерны
 - State - состояние
 """
 
-# State - состояние. Паттерн, который позволяет объекту менять свое поведение в зависимости от своего состояния.
-# Пример: бариста. В зависимости от состояния, он будет готовить кофе, чай или коктейли.
+# State - состояние. 
+
+"""
+1. Класс создающий экземплер Браузера
+2. Класс контекст состояний
+3. Класс сосстояния, в котором браузер идет на сайт book.toscrape.com
+4. Класс сосстояния, в котором браузер идет на сайт Гугл
+5. Фасад для управления состояниями и получения задачи от пользователя __call__
+"""
 
 from abc import ABC, abstractmethod
+from tkinter import N
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-class BaristaState(ABC):
+
+class Chrome():
+    """
+    Класс создающий экземплер Браузера
+    """
+    def __init__(self, settings=None, implicity_wait=10):
+        self.settings = settings
+        self.implicity_wait = implicity_wait
+
+    def get_options_object(self):
+        """
+        Получение объекта настроек для Chrome
+        """
+        options = webdriver.ChromeOptions()
+        if self.settings:
+            for option in self.settings:
+                options.add_argument(option)
+        return options
+
+    def get_driver(self):
+        """
+        Получение драйвера для Chrome
+        """
+        options = self.get_options_object()
+        driver = webdriver.Chrome(options)
+        driver.implicitly_wait(self.implicity_wait)
+        return driver
+    
+    def __call__(self):
+        """
+        Получение драйвера для Chrome
+        """
+        return self.get_driver()
+
+
+
+class Browser:
+    def __init__(self, state: "State", driver: Chrome):
+        self.state = state
+        self.driver = driver
+
+    def change_state(self, state: "State"):
+        self.state = state
+
+    def work(self):
+        self.state.work(self.driver)
+
+
+class State(ABC):
+    def __init__(self, driver: Chrome):
+        self.driver = driver
+
     @abstractmethod
-    def get_work(self):
+    def work(self):
         pass
 
 
-class CoffeeState(BaristaState):
-    def get_work(self):
-        print("Приготовление кофе")
+class BookToscrapeState(State):
+    def work(self):
+        self.driver.get("http://books.toscrape.com")
+        time.sleep(5)
 
 
-class TeaState(BaristaState):
-    def get_work(self):
-        print("Приготовление чая")
+class GoogleState(State):
+    def work(self):
+        self.driver.get("http://google.com")
+        # Ищем поисковую форму title="Поиск"
+        search_form = self.driver.find_element(By.CSS_SELECTOR, "input[name='q']")
+        search_form.send_keys("Котики")
+        search_form.submit()
 
 
-class CocktailState(BaristaState):
-    def get_work(self):
-        print("Приготовление коктейлей")
-
-class CleanState(BaristaState):
-    def get_work(self):
-        print("Уборка")
-
-
-class Barista:
+class MainFacade:
     def __init__(self):
-        self.state = CoffeeState()
+        self.driver = Chrome()()
+        self.state = GoogleState(self.driver)
+        self.browser = Browser(self.state, self.driver)
 
-    def set_state(self, state):
-        self.state = state
+    def __call__(self):
+        while True:
+            task = input("Введите задачу: ")
+            if task == "google":
+                self.browser.change_state(GoogleState(self.driver))
+            elif task == "book":
+                self.browser.change_state(BookToscrapeState(self.driver))
+            elif task == "exit":
+                break
+            self.browser.work()
 
-    def get_work(self):
-        self.state.get_work()
 
-
-barista = Barista()
-cocktail_state = CocktailState()
-tea_state = TeaState()
-clean_state = CleanState()
-
-barista.set_state(cocktail_state)
-barista.get_work()
+if __name__ == "__main__":
+    MainFacade()()
