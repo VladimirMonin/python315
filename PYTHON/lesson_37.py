34645f4c079df341a4a7e4d27394ae951e206575
v <v@mail.ru>
2024-04-19 17:20:10 +0500

    lesson_37: проблема assert

--- PYTHON/lesson_37.py
+++ PYTHON/lesson_37.py
@@ -7,16 +7,71 @@ assert - проверка условия
 assert exception - проверка исключения
 """
 
-is_happy = False
+city_dict = {
+    'name': 'Moscow',
+    'population': '13143',
+    'area': '13143',
+    'country': 'Russia',
+}
 
-# assert is_happy, "Похоже, что я не счастлив"
-
-def test_is_happy():
-    try:
-        assert is_happy, "Похоже, что я не счастлив"
-    
-    except AssertionError as e:
-        print(e)
+def validate_name(name):
+    """
+    Проверка наличия имени
+    """
+    if isinstance(name, str):
+        return True
+    else:
         return False
     
-test_is_happy()
+def validate_population(population):
+    """
+    Проверка наличия населения
+    """
+    if isinstance(population, int):
+        return True
+    else:
+        return False
+    
+
+def validate_area(area):
+    """
+    Проверка наличия площади
+    """
+    if isinstance(area, int):
+        return True
+    else:
+        return False
+    
+
+# if validate_name(city_dict['name']):
+#     print('Имя города валидно')
+# else:
+#     raise ValueError('Имя города не валидно')
+
+# if validate_population(city_dict['population']):
+#     print('Население города валидно')
+# else:
+#     raise ValueError('Население города не валидно')
+
+# if validate_area(city_dict['area']):
+#     print('Площадь города валидна')
+# else:
+#     raise ValueError('Площадь города не валидна')
+
+
+assert validate_name(city_dict['name']) == True, 'Имя города не валидно'
+assert validate_population(city_dict['population']) == True, 'Население города не валидно'
+assert validate_area(city_dict['area']) == True, 'Площадь города не валидна'
+
+
+"""
+Ключевая проблема подобного кода - изобретение велосипеда заново.
+Эти тесты будут останавливаться полностью, на первом же неудачном случае.
+
+Так же, нам придется писать управляющий модуль, который будет собирать тесты с 
+разных файлов и запускать их. Попутно решая огромное количество проблем.
+
+Pytest - это фреймворк для тестирования, который позволяет писать тесты в
+удобном и понятном виде, а так же управлять ими.
+"""
+
