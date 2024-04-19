b6338e6fee09e65f9da91b1c0f6e29313e0ee26d
v <v@mail.ru>
2024-04-19 18:17:38 +0500

    lesson_37: параметризованные тесты parametrize

--- PYTHON/lesson_37_test.py
+++ PYTHON/lesson_37_test.py
@@ -9,7 +9,12 @@ assert exception - проверка исключения
 тестовые функции должны начинаться с test_
 файлы с тестами должны начинаться с test_ или заканчиваться _test
 где и как должны лежать тесты?
+@pytest.mark.slow - маркировка тестов slow - придуманный маркер
+Запуск тестов с маркером pytest -m "slow"
+Запуск тестов без маркера pytest -m "not slow" (подробнее в конспекте)
+@pytest.mark.parametrize - параметризация тестов
 """
+import pytest
 
 def is_palindrome(word):
     return word == word[::-1]
@@ -20,5 +25,22 @@ def test_is_palindrome_registr():
     # ТАК НЕ НАДО. Потому что, если тест упадет, то дальше не пойдет
     assert is_palindrome('а роза упала на лапу азора') == True, 'Ваша функция не обрабатывает многословные палиндромы'
 
+
+
+@pytest.mark.slow
 def test_is_palindrome_multiple_words():
     assert is_palindrome('а роза упала на лапу азора') == True, 'Ваша функция не обрабатывает многословные палиндромы'
+
+
+test_set = [
+    ('дуд', True),
+    ('дудка', False),
+    ('банан', False),
+    ('bob', True),
+    ('bobik', False),
+    ('бобер', False),
+]
+
+@pytest.mark.parametrize('word, result', test_set)
+def test_is_palindrome_param(word, result):
+    assert is_palindrome(word) == result, f'Слово {word} не является палиндромом'
