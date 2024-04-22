4020be7068c33e6f53c726bea7ff4a9f1e84776d
v <v@mail.ru>
2024-04-22 20:33:45 +0500

    lesson_38: разбор HW

--- PYTHON/lesson_38_test.py
+++ PYTHON/lesson_38_test.py
@@ -3,48 +3,142 @@ Lesson 38
 Pytest
 - Фикстуры
 - Параметризация фикстур
+- Разбор HW с погодным API
 """
+
 import pytest
-import json
-
-def is_palindrome(word) -> bool:
-    return word.lower().replace(' ', '') == word[::-1].lower().replace(' ', '')
-
-@pytest.fixture
-def json_data(request):
-    json_str = """[
-    ["дуд", true],
-    ["дудка", false],
-    ["банан", false],
-    ["bob", true],
-    ["bobik", false],
-    ["бобер", false]
-    ]"""
-    data = json.loads(json_str)
-    word, result = data[request.param]
-    return word, result
+import requests
 
 
-@pytest.mark.parametrize('json_data', range(6), indirect=True)
-def test_is_palindrome_param(json_data):
-    word, result = json_data
-    assert is_palindrome(word) == result, f'Слово "{word}" не является палиндромом'
+API_KEY = "23496c2a58b99648af590ee8a29c5348"
+UNITS = "metric"
+LANG = "ru"
+
+class WeatherRequest:
+    def __init__(self, api_key: str = API_KEY, units: str = UNITS, lang: str = LANG):
+        self.city: None | str = None
+        self.api_key = api_key
+        self.units = units
+        self.lang = lang
+        self.url: None | str = None
+        self.data: None | dict = None
+    
+
+    def __get_url(self):
+        self.url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units={self.units}&lang={self.lang}"
+        
+    
+    def __get_data(self):
+        self.data: dict = requests.get(self.url).json()
+
+    
+    def __call__(self, city: str) -> dict:
+        self.city = city
+        self.__get_url()
+        self.__get_data()
+        return self.data
+        
+
+
+# Делаем запрос погоды Москва
+wr = WeatherRequest()
+weather_moscow = wr('Москва')
+print(weather_moscow)
+
+
+cities = [  
+    ('Москва', {"lon": 37.6156, "lat": 55.7522}),  
+    ('Воронеж', {"lon": 39.17, "lat": 51.6664}),  
+    ('Санкт-Петербург', {"lon": 30.2642, "lat": 59.8944}),  
+    ('Краснодар', {"lon": 38.9769, "lat": 45.0328}),  
+    ('Сочи', {"lon": 39.7303, "lat": 43.6}),  
+]
+
+
+@pytest.fixture(scope='module')
+def weather_request():
+    """
+    Создает экземпляр класса WeatherRequest. Делает запрос погоды для Москвы
+    Возвращает словарь с данными о погоде
+    """
+    return WeatherRequest(api_key=API_KEY, units=UNITS, lang=LANG)('Москва')
+
+
+@pytest.fixture(scope='module')
+def weather_request_parametrize():
+    """
+    Создает экземпляр класса WeatherRequest и возвращает его
+    Тестовая функция будет принимать параметр city_name и делать запрос погоды для нужного города
+    """
+    return WeatherRequest(api_key=API_KEY, units=UNITS, lang=LANG)
+
+
+def test_weather_request_city_name(weather_request):
+    assert weather_request['name'] == 'Москва', 'Название города не соответствует ожидаемому'
+
+
+def test_weather_request_coord(weather_request):
+    assert weather_request['coord'] == {"lon": 37.6156, "lat": 55.7522}, 'Координаты города не соответствуют ожидаемым'
+
+
+def test_weather_request_weather_key(weather_request):
+    assert set(weather_request['weather'][0].keys()) == {'id', 'main', 'description', 'icon'}, 'Отсутствуют ключи в секции weather'
+
+def test_weather_request_main_key(weather_request):
+    assert set(weather_request['main'].keys()) == {'temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity', 'sea_level',
+'grnd_level'}, 'Отсутствуют ключи в секции main'
+
+@pytest.mark.slow
+@pytest.mark.parametrize('city_name, expected_coords', cities)
+def test_weather_request_city_coodrd_name_parametrize_slow(weather_request_parametrize, city_name, expected_coords):
+    assert weather_request_parametrize(city_name)['coord'] == expected_coords, f'Координаты города {city_name} не соответствуют ожидаемым'
+
+"""
+Команды запуска:
+Все тесты: pytest
+Только тесты с медленным тегом: pytest -m slow
+Тесты без тега slow: pytest -m "not slow"
+Только тесты с параметризацией: pytest -k parametrize
+"""
 
 
 """
-Подробное объяснение:
-
-@pytest.fixture: Эта декорация определяет фикстуру. Фикстура — это функция, которая запускается перед каждым тестом, в котором она используется. 
-
-Фикстура json_data загружает данные из строки JSON и возвращает пару значений (слово и ожидаемый результат) на основе request.param.
-
-request.param: Это специальный параметр, который предоставляется pytest для доступа к параметрам теста в фикстуре. Он используется для того, чтобы выбрать нужный элемент из списка данных. Каждый раз при выполнении теста, pytest изменяет значение request.param в соответствии с заданным в parametrize.
-
-@pytest.mark.parametrize: 'json_data': имя параметра теста.
-
-range(6): создает последовательность индексов от 0 до 5, что соответствует числу элементов в данных JSON.
-
-indirect=True: указывает pytest, что значения в range(6) должны быть переданы не напрямую в тест, а через фикстуру json_data, которая использует эти значения для выбора нужных данных.
-
-Тестовая функция test_is_palindrome_param: принимает результаты из фикстуры json_data (слово и результат) и проверяет, является ли слово палиндромом. Сообщение об ошибке указывает, что слово должно быть палиндромом, если тест не проходит.
+Практика!
+Создайте функцию сложения двух чисел
+Создайте тест для этой функции с параметризацией
 """
+
+sum_data_set = [
+    (2, 2, 4),
+    (3, 3, 6),
+    (4, 4, 8),
+    (5, 5, 10),
+    (6, 6, 12),
+    (100, 100, 200),
+    (0, 0, 0),
+    (-1, 1, 0),
+    (-1, -1, -2),
+    (1, -1, 0),
+    (4, -3, 1),
+    (-20, 20, 0)
+]
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+@pytest.mark.parametrize('a, b, result', sum_data_set)
+def test_sum(a, b, result):
+    assert sum(a, b) == result, f'Сумма чисел {a} и {b} не равна {result}'
+
+
+# Фикстура которая вернет функцию сложения
+
+@pytest.fixture(scope='module')
+def sum_fixture():
+    return sum
+
+# Параметризация и фикстура
+
+@pytest.mark.parametrize('a, b, result', sum_data_set)
+def test_sum_fixture(sum_fixture, a, b, result):
+    assert sum_fixture(a, b) == result, f'Сумма чисел {a} и {b} не равна {result}'
